d58528993986778961e295f457f05dd95fb2c34a
GabrielECoy <40899132+GabrielECoy@users.noreply.github.com>
2025-06-26 21:50:11 +0000

    Adding example extended by copilot and rest request response as file

--- Ex_Files/04_01_end/main.py
+++ Ex_Files/04_01_end/main.py
@@ -1,10 +1,22 @@
 import requests
+from pprint import pprint
 
 response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
-last_twenty_years = response.json()[1][:20]
+data = response.json()[1][:50]  # Get last 50 years
 
-for year in last_twenty_years:
-    display_width = year["value"] // 10_000_000
-    print(f'{year["date"]}: {year["value"]}', "=" * display_width)
+def safe_value(val):
+    return val if val is not None else 0
+
+# Calculate 5-year averages
+for i in range(0, 50, 5):
+    group = data[i:i+5]
+    values = [year["value"] for year in group if year["value"] is not None]
+    if values:
+        avg = sum(values) // len(values)
+        years = f'{group[-1]["date"]}-{group[0]["date"]}'
+        print(f"{years}: 5-year average population: {avg}","Years with values",len(values))
+    else:
+        years = f'{group[-1]["date"]}-{group[0]["date"]}'
+        print(f"{years}: No data for 5-year average")
